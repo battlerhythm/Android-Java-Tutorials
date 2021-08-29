be04ec2c5381cb84ba3d88cd871ab33feb5d6002
Jeongmin Oh <16344965+battlerhythm@users.noreply.github.com>
2021-08-29 19:53:46 -0400

    Update

---
+++ HelloToast/README.md
@@ -0,0 +1,2 @@
+# Android-Java-HelloToast
+[![Android CI](https://github.com/battlerhythm/Android-Java-HelloToast/actions/workflows/android.yml/badge.svg)](https://github.com/battlerhythm/Android-Java-HelloToast/actions/workflows/android.yml)
---
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.hellotoast;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+  private int mCount = 0;
+  private TextView mShowCount;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    mShowCount = findViewById(R.id.show_count);
+    if (savedInstanceState != null && savedInstanceState.containsKey("count")) {
+      mCount = savedInstanceState.getInt("count");
+      mShowCount.setText(Integer.toString(mCount));
+    }
+  }
+
+  @Override
+  protected void onSaveInstanceState(@NonNull Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putInt("count", mCount);
+  }
+
+  public void showToast(View view) {
+    Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+    toast.show();
+  }
+
+  public void countUp(View view) {
+    mCount++;
+    if (mShowCount != null) {
+      mShowCount.setText(Integer.toString(mCount));
+    }
+  }
+}
